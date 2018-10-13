717f9cdc5208662c29ec268e0add98ee74dcfaad
Camilo Rossi <camilo@hukucha.com>
2018-10-13 15:52:23 -0400

    Recréation du fichier Categories.swift
    Recréation du nouveau storyboard aussi

---
+++ Aeris/Categories.swift
@@ -0,0 +1,21 @@
+//
+//  Categories.swift
+//  Aeris
+//
+//  Created by Camilo Rossi on 2018-10-13.
+//  Copyright © 2018 Camilo Rossi. All rights reserved.
+//
+
+import UIKit
+
+class Categories: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
