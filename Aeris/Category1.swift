3334ffb70f61afc697feaf8ce48ed83a6947dbaf
Camilo Rossi <camilo@hukucha.com>
2018-10-13 18:40:39 -0400

    Summary

--- Aeris/Category1.swift
+++ Aeris/Category1.swift
@@ -16,22 +16,93 @@ class Category1: UIViewController {
     @IBOutlet var answer3: UIButton!
     @IBOutlet var lblProgress: UILabel!
     
+    struct Question {
+        let question: String
+        let answers: [String]
+        let correctAnswer: Int
+    }
     
+    
+    var questions: [Question] = [
+        Question(
+            question: "What is 1+1?",
+            answers: ["1", "2", "3", "4"],
+            correctAnswer: 1),
+        Question(
+            question: "Have you subscrbed to Seemu Apps",
+            answers: ["Yes", "No", "I will", "No Thanks"],
+            correctAnswer: 0),
+        Question(
+            question: "What is the Capital of Australia?",
+            answers: ["Sydney", "Melbourne", "Adelaide", "Canberra"],
+            correctAnswer: 3)
+    ]
+    
+    var currentQuestion: Question?
+    var currentQuestionPos = 0
+    
+    var noCorrect = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        currentQuestion = questions[0]
+        setQuestion()
     }
 
     @IBAction func submitAnswer0(_ sender: Any) {
+        checkAnswer(idx: 0)
     }
     @IBAction func submitAnswer1(_ sender: Any) {
+        checkAnswer(idx: 1)
     }
     @IBAction func submitAnswer2(_ sender: Any) {
+        checkAnswer(idx: 2)
     }
     @IBAction func submitAnswer3(_ sender: Any) {
+        checkAnswer(idx: 3)
     }
     
+    // Check if an answer is correct then load the next question
+    func checkAnswer(idx: Int) {
+        if(idx == currentQuestion!.correctAnswer) {
+            noCorrect += 1
+        }
+        loadNextQuestion()
+    }
+    
+    func loadNextQuestion() {
+        // Show next question
+        if(currentQuestionPos + 1 < questions.count) {
+            currentQuestionPos += 1
+            currentQuestion = questions[currentQuestionPos]
+            setQuestion()
+            // If there are no more questions show the results
+        } else {
+            performSegue(withIdentifier: "fromCategory1ToResults", sender: nil)
+        }
+        
+        
+    }
+    
+    // Set labels and buttions for the current question
+    func setQuestion() {
+        lblQuestion.text = currentQuestion!.question
+        answer0.setTitle(currentQuestion!.answers[0], for: .normal)
+        answer1.setTitle(currentQuestion!.answers[1], for: .normal)
+        answer2.setTitle(currentQuestion!.answers[2], for: .normal)
+        answer3.setTitle(currentQuestion!.answers[3], for: .normal)
+        lblProgress.text = "\(currentQuestionPos + 1) / \(questions.count)"
+    }
+    
+    // Before we move to the results screen pass the how many we got correct, and the total number of questions
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if(segue.identifier == "fromCategory1ToResults") {
+            var vc = segue.destination as! ResultsViewController
+            vc.noCorrect = noCorrect
+            vc.total = questions.count
+        }
+    }
 
 }
 
--- Aeris/ResultsViewController.swift
+++ Aeris/ResultsViewController.swift
@@ -9,11 +9,32 @@
 import UIKit
 
 class ResultsViewController: UIViewController {
-
+    @IBOutlet var lblTitle: UILabel!
+    @IBOutlet var lblResults: UILabel!
+    
+    var noCorrect = 0
+    var total = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // Set the results
+        lblResults.text = "You got \(noCorrect) out of \(total) correct"
+        
+        // Calculate the percentage of quesitons you got right
+        var percentRight = Double(noCorrect) / Double(total)
+        percentRight *= 100
+        
+        // Based on the percentage of questions you got right present the user with different message
+        var title = ""
+        if(percentRight < 40) {
+            title = "Not Good"
+        } else if(percentRight < 70) {
+            title = "Almost"
+        } else {
+            title = "Good Job"
+        }
+        lblTitle.text = title
     }
     
 
