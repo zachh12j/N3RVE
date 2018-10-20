29176c451fa49bad8701c86d643a6fd772d0130e
Camilo Rossi <camilo@hukucha.com>
2018-10-20 17:15:47 -0400

    Update Aeris/SplashScreen.swift

--- Aeris/SplashScreen.swift
+++ Aeris/SplashScreen.swift
@@ -8,7 +8,37 @@
 
 import Foundation
 import UIKit
+import AVFoundation
+import AVKit
 
 class SplashScreen: UIViewController{
+    
+    var avPlayer: AVPlayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if let path = Bundle.main.path(forResource: "Introduction", ofType: "mp4")
+        {
+            let video = AVPlayer(url: URL(fileURLWithPath: path))
+            let videoPlayer = AVPlayerViewController()
+            videoPlayer.player = video
+            videoPlayer.showsPlaybackControls = false
+            
+            present(videoPlayer, animated: true, completion:
+                {
+                    video.play()
+            })
+        }
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(Settings.finishVideo), name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: nil)
+    }
+    
+    @objc func finishVideo()
+    {
+        print("Video Finished")
+    }
     
 }
